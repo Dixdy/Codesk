df2298cc76204ba147e2f9b88ca7565ad18f3acb
Dixdy <nicholaslow21@yahoo.com.sg>
2021-03-03 14:44:46 +0800

    bug fixes

--- Codesk/userprofile/views.py
+++ Codesk/userprofile/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 
 def Profile(request):
     if request.method == "POST":
-        print(request.POST)
         if 'searchword' in request.POST:
-            print(request.POST.get('search'))
-            context = {'word' : request.POST.get('search')}
+            searchkeyword = request.POST.get('search')
+            print(searchkeyword)
+            #context = databasefunction(searchkeyword)
+            context = {'word':searchkeyword}
             print(context)
             return render(request, 'home.html', context)
         context = {'text' : "Your profile has been edited!"}
