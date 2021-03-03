ce4615f551a03842c9c09007c9c60da68ba21ab1
yuuchennn <c190172@e.ntu.edu.sg>
2021-03-03 15:31:37 +0800

    Database

---
+++ Codesk/API/database.py
@@ -0,0 +1,65 @@
+from django.contrib.auth.models import User as u
+from accounts.models import Question as q, QuesionComment as qc, Answer as a, AnswerComment as ac, Notes as n
+from django.db.models import Q
+from functools import reduce 
+
+#Add a question into database using coursecode, title, content, author input
+#author can be passed using request.user
+def addQuestion(cc, tle, cnt, aut):
+    newQuestion = q(coursecode=cc, title=tle, content=cnt, author=aut)
+    newQuestion.save()
+
+#Search for a list of question using user's input
+def searchQuestion(input):
+    keywords = input.split()
+    queries = [Q(content__contains=i) for i in keywords]
+    query = reduce(lambda x, y: x & y, queries)
+    return q.objects.filter(query)
+    #return a list of Question objects
+
+#Search for the comment of the question
+#qn refer to the content of the question
+def searchQuestionComment(qn):
+    return qc.objects.filter(belongs_to=q.objects.get(content=qn))
+    #return a list of QuestionComment objects
+
+#Add an answer to a question using content, belongs_to and author
+#belongs_to refer to the content of the question
+def addAnswer(cnt, qn, aut):
+    newAnswer = a(content=cnt, belongs_to=q.objects.get(content=qn), author=aut)
+    newAnswer.save()
+
+#Search for the answer of the question
+#qn refer to the content of the question  
+def searchAnswer(qn):
+    return a.objects.filter(belongs_to=q.objects.get(content=qn))
+    #return a list of Answer objects
+
+#Search for the comment of the answer
+#ans refer to the content of the answer
+def searchAnswerComment(ans):
+    return ac.objects.filter(content=a.objects.get(content=ans))
+
+#Adding a new notes
+def addNotes(cc, tle, cnt, aut):
+    newNotes = n(coursecode=cc, title=tle, content=cnt, author=aut)
+    newNotes.save()
+
+def searchNotes(input):
+    keywords = input.split()
+    queries = [Q(content__contains=i) for i in keywords]
+    query = reduce(lambda x, y: x & y, queries)
+    return n.objects.filter(query)
+
+#nts refer to the content of the notes used to identify the notes to upvotes
+def upVotes(nts):
+    a = n.objects.filter(content=nts)[0:1]
+    a.votes = a.votes + 1
+    a.save()
+
+#nts refer to the content of the notes used to identify the notes to downvotes
+def downVotes(nts):
+    a = n.objects.filter(content=nts)[0:1]
+    if a.votes>0:
+        a.votes = a.votes - 1
+        a.save()
--- Codesk/accounts/models.py
+++ Codesk/accounts/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    coursecode = models.CharField(default='XXXXXX', max_length=8)
+    title = models.TextField()
+    content = models.TextField()
+    date_posted = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+class QuestionComment(models.Model):
+    content = models.TextField()
+    date_posted = models.DateField(default=timezone.now)
+    belongs_to = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+class Answer(models.Model):
+    content = models.TextField()
+    date_posted = models.DateField(default=timezone.now)
+    votes = models.IntegerField(default='0')
+    belongs_to = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+class AnswerComment(models.Model):
+    content = models.TextField()
+    date_posted = models.DateField(default=timezone.now)
+    votes = models.IntegerField(default='0')
+    belongs_to = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+class Notes(models.Model):
+    coursecode = models.CharField(max_length=8)
+    title = models.TextField()
+    content = models.TextField()
+    date_posted = models.DateField(default=timezone.now)
+    votes = models.IntegerField(default='0')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
