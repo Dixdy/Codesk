44ab4e80b1eef39c50acf5d55f5dcb650b7d1ccf
Dixdy <nicholaslow21@yahoo.com.sg>
2021-03-05 18:20:59 +0800

    styling for login,logout,home,register; fixed home search

--- Codesk/accounts/urls.py
+++ Codesk/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, logout_request
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('logout/', logout_request, name='logout'),
 ]
--- Codesk/accounts/views.py
+++ Codesk/accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
 
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
@@ -10,3 +12,9 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def logout_request(request):
+    username = request.user
+    messages.info(request, f"{username}, you have successfully logged out!")
+    logout(request)
+    return redirect('home')
