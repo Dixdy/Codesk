8a0dcf192911bb233cb00348ad82bf1bea0a0a4a
Dixdy <nicholaslow21@yahoo.com.sg>
2021-03-11 14:50:44 +0800

    fixed all searchbars & styled login

--- Codesk/accounts/views.py
+++ Codesk/accounts/views.py
@@ -1,32 +1,41 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login ,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
-# Create your views here.
+from API import search
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
 
-
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+def registerUser(request):
+    if request.method == 'POST':
+        if 'searchword' in request.POST:
+            context = search.searchbar(request)
+            return render(request, 'results.html', context)
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 def logout_request(request):
     username = request.user
-    messages.info(request, f"{username}, you have successfully logged out!")
+    messages.success(request, f"{username}, you have successfully logged out!")
     logout(request)
     return redirect('home')
 
 def userLogin(request):
+    context = {}
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        if 'searchword' in request.POST:
+            context = search.searchbar(request)
+            return render(request, 'results.html', context)
+        username = request.POST.get('uname')
+        password = request.POST.get('psw')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.info(request, 'Login failed')
-            print("zxzycxz")
+            context = {'msg':'Failed'}
+    return render(request, 'registration/login.html', context)
+
